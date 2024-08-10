701e800d5c4a9b9aa14fbbe04d357b6edd258f03
Technerd1124 <Technerd1124@gmail.com>
2024-08-10 18:41:03 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#autotype', {
+    strings: [' C', 'C++','Java','SQL','Web Development'],
+    typeSpeed:130,
+    backSpeed: 30,
+    loop:true
+  });
+ // setInterval( Typed(), 200);
