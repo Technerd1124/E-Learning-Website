2d6167815ee480768873e5c1e542e2d51caf8293
Technerd1124 <Technerd1124@gmail.com>
2024-11-03 17:39:16 +0530

    feedback change

--- FeedBack/feedback.js
+++ FeedBack/feedback.js
@@ -1,17 +1,16 @@
 function sendMail() {
-   var perms = {
-       name: document.getElementById("username").value,
-       email: document.getElementById("email").value,
-       message: document.getElementById("feedback").value
-   };
+    var perms = {
+        name: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("feedback").value
+    };
 
-   emailjs.send("service_23wzqfb", "template_spsoxnd", perms)
-       .then(function(response) {
-           alert("Email sent successfully!");
-           console.log("SUCCESS!", response.status, response.text);
-       })
-       .catch(function(error) {
-           alert("Failed to send email.");
-           console.error("FAILED...", error);
-       });
+    emailjs.send("service_23wzqfb", "template_spsoxnd", perms)
+        .then(function(response) {
+            alert("Email Sent!");
+            console.log("Email sent successfully", response.status, response.text);
+        }, function(error) {
+            alert("Failed to send email.");
+            console.error("Failed to send email", error);
+        });
 }
